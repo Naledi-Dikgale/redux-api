12a55e8b03b8259e49e4c03c915eb0b4cea7a243
naledi-dikgale <naledi22khosi@gmail.com>
2023-05-10 15:25:25 +0200

    Initialize project with create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React 18 Alpha</h1>
+    </div>
+  );
+}
+
+export default App;
